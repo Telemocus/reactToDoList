1631e0e1d0d0f4b12127a392e937f24b841837de
Mocus <mocussmncube@gmail.com>
2024-02-20 16:00:36 +0200

    Completed move functions

--- src/TodoList/ToDoList.jsx
+++ src/TodoList/ToDoList.jsx
@@ -21,11 +21,21 @@ function ToDoList(){
      }
 
      function moveTaskUp(index) {
-
+          if(index > 0 ){
+               const updatedTasks = [...tasks];
+               [updatedTasks[index], updatedTasks[index - 1]] =
+               [updatedTasks[index - 1], updatedTasks[index]];
+               setTasks(updatedTasks);
+          }
      }
 
      function moveTaskDown(index){
-
+           if(index < tasks.length - 1 ){
+               const updatedTasks = [...tasks];
+               [updatedTasks[index], updatedTasks[index + 1]] =
+               [updatedTasks[index + 1], updatedTasks[index]];
+               setTasks(updatedTasks);
+          }
      }
      return(
           <div className="to-do-list">
